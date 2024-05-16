48e48cbcfb33c7e5322fc0f54ad496a3f7bb99c7
adenafil <nafilie9@gmail.com>
2024-05-16 09:12:48 +0700

    update dump database

--- dump_database/dump-creativehub-202405160912.sql
+++ dump_database/dump-creativehub-202405160912.sql
@@ -25,8 +25,9 @@ DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,6 +36,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (3,'ebook'),(4,'font'),(2,'icon'),(1,'template'),(5,'ui kit');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,7 +52,7 @@ CREATE TABLE `chart_products` (
   `chart_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `chart_id` (`chart_id`),
+  UNIQUE KEY `unique_chart_products` (`chart_id`,`product_id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `chart_products_ibfk_1` FOREIGN KEY (`chart_id`) REFERENCES `charts` (`id`),
   CONSTRAINT `chart_products_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
@@ -102,7 +104,7 @@ CREATE TABLE `images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `location` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -111,35 +113,67 @@ CREATE TABLE `images` (
 
 LOCK TABLES `images` WRITE;
 /*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'www.google.com/hasan'),(2,'www.google.com/husain'),(3,'www.google.com/ali'),(4,'www.product.com/1'),(5,'www.product.com/2'),(6,'www.product.com/3'),(7,'www.buktipembayaran.com/bri.png');
 /*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `payment_account`
+-- Table structure for table `payment`
 --
 
-DROP TABLE IF EXISTS `payment_account`;
+DROP TABLE IF EXISTS `payment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `payment_account` (
+CREATE TABLE `payment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `bank_name` varchar(255) DEFAULT NULL,
-  `bank_number` int(11) DEFAULT NULL,
+  `bank_name` varchar(100) DEFAULT NULL,
+  `account_name` varchar(100) DEFAULT NULL,
+  `account_number` varchar(50) DEFAULT NULL,
+  `image_id` int(11) DEFAULT NULL,
+  `transaction_id` int(11) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `bank_number` (`bank_number`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `payment_account_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `image_id` (`image_id`),
+  KEY `transaction_id` (`transaction_id`),
+  CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`),
+  CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`transaction_id`) REFERENCES `transactions` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `payment_account`
+-- Dumping data for table `payment`
 --
 
-LOCK TABLES `payment_account` WRITE;
-/*!40000 ALTER TABLE `payment_account` DISABLE KEYS */;
-/*!40000 ALTER TABLE `payment_account` ENABLE KEYS */;
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+INSERT INTO `payment` VALUES (1,'BRI','hasan','3012210002',7,1,'DONE','2024-05-16 02:10:07','2024-05-16 02:10:07');
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_asset`
+--
+
+DROP TABLE IF EXISTS `product_asset`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_asset` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `location` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_asset`
+--
+
+LOCK TABLES `product_asset` WRITE;
+/*!40000 ALTER TABLE `product_asset` DISABLE KEYS */;
+INSERT INTO `product_asset` VALUES (1,'www.myproduct.com/asset/myasset1.png'),(2,'www.myproduct.com/asset/myasset2.png'),(3,'www.myproduct.com/asset/myasset3.png'),(4,'www.myproduct.com/asset/myasset4.png');
+/*!40000 ALTER TABLE `product_asset` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -155,16 +189,19 @@ CREATE TABLE `products` (
   `image_id` int(11) DEFAULT NULL,
   `seller_id` int(11) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
+  `asset_id` int(11) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `category_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `image_id` (`image_id`),
   KEY `seller_id` (`seller_id`),
+  KEY `asset_id` (`asset_id`),
   KEY `category_id` (`category_id`),
   CONSTRAINT `products_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`),
   CONSTRAINT `products_ibfk_2` FOREIGN KEY (`seller_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `products_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CONSTRAINT `products_ibfk_3` FOREIGN KEY (`asset_id`) REFERENCES `product_asset` (`id`),
+  CONSTRAINT `products_ibfk_4` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -173,6 +210,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Icon Pack Web UISI',4,3,'Saya baru saja sciping semua icon uisi',1,5000000,2),(2,'Template Web UISI',5,3,'Saya baru saja hack web uisi',2,5000000,1),(3,'Ebook Sejarah Abu Hasan Asy-Syadzili',6,3,'ebook sejarah Abu Hasan Asy-Syadzili',3,3000000,3),(4,'Font Jawir',6,3,'font yang bisa berbahsa jawa',4,1000000,4);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -189,13 +227,14 @@ CREATE TABLE `reviews` (
   `comment` text DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
+  UNIQUE KEY `unique_user_reviews` (`user_id`,`product_id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -204,6 +243,7 @@ CREATE TABLE `reviews` (
 
 LOCK TABLES `reviews` WRITE;
 /*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
+INSERT INTO `reviews` VALUES (1,5,'Ya Maula Ya Ali, ane suka product ente',1,2,'2024-05-16 02:10:25','2024-05-16 02:10:25');
 /*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -218,14 +258,13 @@ CREATE TABLE `transactions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `total` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `product_id` (`product_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
   CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -234,6 +273,7 @@ CREATE TABLE `transactions` (
 
 LOCK TABLES `transactions` WRITE;
 /*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (1,2,1,'2024-05-16 02:05:29');
 /*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -260,7 +300,7 @@ CREATE TABLE `user_details` (
   KEY `image_id` (`image_id`),
   CONSTRAINT `user_details_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `user_details_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -269,6 +309,7 @@ CREATE TABLE `user_details` (
 
 LOCK TABLES `user_details` WRITE;
 /*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
+INSERT INTO `user_details` VALUES (1,1,'hasan@gmail.com','12345678','achmad hasan',NULL,NULL,1,'2024-05-16 02:04:08'),(2,2,'husain@gmail.com','12345678','achmad husain',NULL,NULL,2,'2024-05-16 02:04:10'),(3,3,'ali@gmail.com','12345678','ali',NULL,NULL,3,'2024-05-16 02:04:12');
 /*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -284,9 +325,10 @@ CREATE TABLE `user_roles` (
   `role` varchar(255) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `user_roles_unique` (`role`,`user_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -295,6 +337,7 @@ CREATE TABLE `user_roles` (
 
 LOCK TABLES `user_roles` WRITE;
 /*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (2,'buyer',2),(3,'buyer',3),(1,'seller',1);
 /*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -311,7 +354,7 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -320,6 +363,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'hasan','2024-05-16 02:03:57'),(2,'husain','2024-05-16 02:03:57'),(3,'ali','2024-05-16 02:03:57');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -336,7 +380,7 @@ CREATE TABLE `wishlist` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `wishlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -345,6 +389,7 @@ CREATE TABLE `wishlist` (
 
 LOCK TABLES `wishlist` WRITE;
 /*!40000 ALTER TABLE `wishlist` DISABLE KEYS */;
+INSERT INTO `wishlist` VALUES (1,1);
 /*!40000 ALTER TABLE `wishlist` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -360,11 +405,11 @@ CREATE TABLE `wishlist_products` (
   `wishlist_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `wishlist_id` (`wishlist_id`),
+  UNIQUE KEY `unique_wishlist_products` (`wishlist_id`,`product_id`),
   KEY `product_id` (`product_id`),
   CONSTRAINT `wishlist_products_ibfk_1` FOREIGN KEY (`wishlist_id`) REFERENCES `wishlist` (`id`),
   CONSTRAINT `wishlist_products_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -373,6 +418,7 @@ CREATE TABLE `wishlist_products` (
 
 LOCK TABLES `wishlist_products` WRITE;
 /*!40000 ALTER TABLE `wishlist_products` DISABLE KEYS */;
+INSERT INTO `wishlist_products` VALUES (2,1,2),(1,1,3);
 /*!40000 ALTER TABLE `wishlist_products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -389,4 +435,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-15 15:56:39
+-- Dump completed on 2024-05-16  9:12:03
